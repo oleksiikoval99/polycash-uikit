931320a82f29fbe32527633a2ebf2bf59c76fce9
PolyCash <polycash.finance@gmail.com>
2021-06-02 22:14:52 +0200

    fix: colors

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2803,10 +2803,10 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#7bb145",
-    primaryBright: "#7bb145",
-    primaryDark: "#7bb145",
-    secondary: "#377223",
+    primary: "#9FCC2E",
+    primaryBright: "#9FCC2E",
+    primaryDark: "#9FCC2E",
+    secondary: "#7BB145",
     success: "#31D0AA",
     warning: "#FFB237",
 };
@@ -2816,7 +2816,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#0E0F19", textDisabled: "#BDC2C4", textSubtle: "#0E0F19", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#377223", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0E0F19", tertiary: "#353547", text: "#9FCC2E", textDisabled: "#666171", textSubtle: "#9FCC2E", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#7BB145", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0E0F19", tertiary: "#353547", text: "#9FCC2E", textDisabled: "#666171", textSubtle: "#9FCC2E", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
